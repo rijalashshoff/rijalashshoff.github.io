e1c4d05a09a07680d20b73affd81a65722da8ead
rijalashshoff <80059652+rijalashshoff@users.noreply.github.com>
2022-07-30 11:48:24 +0700

    Add files via upload

---
+++ port1/script.js
@@ -0,0 +1,6 @@
+document.getElementById("kirim").onclick = function() {
+    var myName = document.getElementById("first").value;
+    var myCity = document.getElementById("last").value;
+
+    document.getElementById("teks").value = "Hello World";
+}
